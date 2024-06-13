b62733e5d8e2c88ac5ece6c2da8ed4504195f815
vijayarkh <146951615+vijayarkh@users.noreply.github.com>
2024-06-13 15:29:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if Ingredients_list:
 Ingredients_string =''
 for fruit_chosen in Ingredients_list:
     Ingredients_string += fruit_chosen + ' '
-    st.subheader(fruit_chosen + 'Nutrition_Information')
+    st.subheader(fruit_chosen + ' Nutrition Information')
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
     fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 st.write(Ingredients_string)
